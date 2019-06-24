a89360179b64ec0db7eda8c8138341e493ea479b
Rafael Jáber <rafaeljaber@MacBook-Pro-de-Rafael.local>
2019-06-24 11:01:32 -0300

    First commit

--- home/views.py
+++ home/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 
+# TODO: Refatorar para usar threads assim que possivel
 def home(request):
     return render(request, 'home.html')
 
 
+# FIXME: Corrigir bug ....
 def my_logout(request):
     logout(request)
     return redirect('home')
