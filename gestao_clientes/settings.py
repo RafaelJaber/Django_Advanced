6e56344063262825419e89e40a1377116568f031
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-04 07:48:28 -0300

    Configuração do Banco de dados para o PostgreSQL

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
 """
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('USER_SQL'),
         'PASSWORD': config('PASS_SQL'),
