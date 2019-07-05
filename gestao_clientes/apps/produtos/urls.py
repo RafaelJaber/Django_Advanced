3f937371760c04e55d49120011d23ff38228a3c3
Rafael Jáber <rafael.jaber@hotmail.com>
2019-07-05 09:55:49 -0300

    Refatorando as views de Produto para utilização do materialize

--- gestao_clientes/apps/produtos/urls.py
+++ gestao_clientes/apps/produtos/urls.py
@@ -1,7 +1,16 @@
 from django.conf import settings
 from django.urls import path, include
-
+from .views import (
+    ProductList,
+    ProductCreate,
+    ProductDelete,
+    ProductUpdate
+)
 
 urlpatterns = [
     #path('list/', persons_list, name="person_list"),
+    path('', ProductList.as_view(), name="product_list"),
+    path('cadastrar-produto/', ProductCreate.as_view(), name="product_create"),
+    path('produto-delete/<int:pk>', ProductDelete.as_view(), name="product_delete"),
+    path('produto-update/<int:pk>', ProductUpdate.as_view(), name="product_update"),
 ]
--- gestao_clientes/apps/produtos/views.py
+++ gestao_clientes/apps/produtos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
-from django.views.generic import View
+from django.views.generic import View, ListView, CreateView, UpdateView
+from .models import Product
+from django.urls import reverse_lazy
 
 """
 class ProductBulk(View):
@@ -16,3 +18,33 @@ class ProductBulk(View):
         return HttpResponse(request, 'Criado')
 """
 
+
+class ProductList(ListView):
+    model = Product
+
+
+class ProductCreate(CreateView):
+    model = Product
+    fields = ['description', 'price']
+
+    success_url = reverse_lazy('product_list')
+
+
+class ProductDelete(View):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+        if product:
+            product.delete()
+        return redirect('product_list')
+
+
+class ProductUpdate(UpdateView):
+    model = Product
+    fields = ['description', 'price']
+
+    success_url = reverse_lazy('product_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['update'] = True
+        return context
