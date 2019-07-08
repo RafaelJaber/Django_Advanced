5f30215bbf76d53df14aa8f4cf0be699b4fec2d0
Rafael Jáber <rafael.jaber@hotmail.com>
2019-07-08 16:46:57 -0300

    Começando os trabalhos com os tests

--- gestao_clientes/apps/vendas/tests.py
+++ gestao_clientes/apps/vendas/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Sale
 
-# Create your tests here.
+
+class SaleTestCase(TestCase):
+    def setUp(self):
+        Sale.objects.create(number="123")
+
+    def test_verified_number_of_sales(self):
+        assert Sale.objects.all().count() >= 1
